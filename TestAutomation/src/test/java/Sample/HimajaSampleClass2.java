6c974f093ad318ce0933c005f2db3487e1223849
Him@j@1234 <himaja04.selenium@gmail.com>
2022-04-22 07:03:43 +0530

    Himaja Branch added new test

---
+++ TestAutomation/src/test/java/Sample/HimajaSampleClass2.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class HimajaSampleClass2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Himaja Sample Class");
+	}
+
+}
