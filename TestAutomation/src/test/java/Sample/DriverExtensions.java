e864bb4a5272014daec59959f4ca793315e0671f
Him@j@1234 <himaja04.selenium@gmail.com>
2022-04-22 06:56:49 +0530

    add Himaja Sample Class

--- TestAutomation/src/test/java/Sample/DriverExtensions.java
+++ TestAutomation/src/test/java/Sample/DriverExtensions.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.ie.InternetExplorerDriver;
 
 public class DriverExtensions {
 
-   public WebDriver driver;
+   public WebDriver driver; 
 
 	public void launchBrowser(String browserName) {
 
---
+++ TestAutomation/src/test/java/Sample/HimajaSampleClass.java
@@ -0,0 +1,11 @@
+package Sample;
+
+public class HimajaSampleClass {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Himaja Sample Class");
+	}
+
+}
